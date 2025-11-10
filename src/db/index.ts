3f041463a38e23c19b4cfbb96936b39d07bd6255
Ritik Kumar Sharma <ritikcollege@gmail.com>
2025-11-10 12:37:49 +0530

    format with bime

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,11 @@
 import mongoose from "mongoose";
 
 await mongoose.connect(process.env.DATABASE_URL || "").catch((error) => {
-	console.log("Error connecting to database:", error);
+  console.log("Error connecting to database:", error);
 });
 
 const client = mongoose.connection
-	.getClient()
-	.db(process.env.DATABASE_NAME || "");
+  .getClient()
+  .db(process.env.DATABASE_NAME || "");
 
 export { client };
-
-
-
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,13 @@
-import { nextCookies } from 'better-auth/next-js';
-import { betterAuth, type BetterAuthOptions } from "better-auth";
+import { type BetterAuthOptions, betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { nextCookies } from "better-auth/next-js";
 import { client } from "@/db";
 
 export const auth = betterAuth<BetterAuthOptions>({
-	database: mongodbAdapter(client),
-	trustedOrigins: [process.env.CORS_ORIGIN || ""],
-	emailAndPassword: {
-		enabled: true,
-	},
-  plugins: [nextCookies()]
+  database: mongodbAdapter(client),
+  trustedOrigins: [process.env.CORS_ORIGIN || ""],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [nextCookies()],
 });
